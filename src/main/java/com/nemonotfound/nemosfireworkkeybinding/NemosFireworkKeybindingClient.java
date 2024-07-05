5c1686c4caf0d3398889fe000b6ac925ce29f495
NemoNotFound <02grimes-bard@icloud.com>
2024-07-05 21:11:28 +0200

    Add keybinding for firework

---
+++ src/main/java/com/nemonotfound/nemosfireworkkeybinding/NemosFireworkKeybindingClient.java
@@ -0,0 +1,35 @@
+package com.nemonotfound.nemosfireworkkeybinding;
+
+import com.nemonotfound.nemosfireworkkeybinding.network.packet.FireworkKeyPressedPayload;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+import static com.nemonotfound.nemosfireworkkeybinding.NemosFireworkKeybinding.MOD_ID;
+
+public class NemosFireworkKeybindingClient implements ClientModInitializer {
+
+    private static KeyBinding fireworkRocketKeyBinding;
+
+    @Override
+    public void onInitializeClient() {
+        fireworkRocketKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key." + MOD_ID + ".firework",
+                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN,
+                "category." + MOD_ID + ".nemos-firework-keybinding"));
+
+        registerFireworkKeyPressedEventProducer();
+    }
+
+    private void registerFireworkKeyPressedEventProducer() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (fireworkRocketKeyBinding.wasPressed()) {
+                ClientPlayNetworking.send(new FireworkKeyPressedPayload());
+            }
+        });
+    }
+}
